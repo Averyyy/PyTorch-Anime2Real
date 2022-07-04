f6a88b7a2ac4d489f85ace71fe265150c0e2dbf4
Avery <hq443@nyu.edu>
2022-07-04 17:02:44 +0800

    fix: opt.epoch

--- train_r2a2r.py
+++ train_r2a2r.py
@@ -23,6 +23,8 @@ parser.add_argument('--input_nc', type=int, default=3,
                     help='number of channels of input data')
 parser.add_argument('--output_nc', type=int, default=3,
                     help='number of channels of output data')
+parser.add_argument('--epoch', type=int, default=0, help='starting epoch')
+
 parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
 parser.add_argument("--batchSize", type=int, default=1, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
